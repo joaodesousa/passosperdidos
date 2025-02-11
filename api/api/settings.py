13f1d78b549189127140c0a9d16f227253629e4f
João de Sousa <joao@pedrodesousa.pt>
2025-02-11 14:13:22 +0000

    Updated settings.py

--- api/api/settings.py
+++ api/api/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DATABASE_URL'),
+        'HOST': 'postgres',
         'PORT': '5432',
     }
 }
