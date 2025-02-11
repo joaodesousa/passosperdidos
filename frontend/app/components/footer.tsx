dc76961295a47a55624ca88212115edc07bc1822
João de Sousa <joao@pedrodesousa.pt>
2025-02-11 15:44:46 +0000

    Corrected footer

--- frontend/app/components/footer.tsx
+++ frontend/app/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="dark:bg-[#09090B] pt-10 pb-6">
       <div className="flex flex-col items-center justify-center text-xs dark:text-white dark:text-opacity-50">
-        <p>Informação obtida através do site da <Link href="https://parlamento.pt" target="_blank" className="text-blue-900">Assembleia da República</Link>.</p>
+        <p>Informação obtida através do site da <Link href="https://www.parlamento.pt" target="_blank" className="text-blue-900">Assembleia da República</Link>.</p>
         <p><Link href="mailto:geral@passosperdidos.pt" target="_blank" className="text-blue-900">Contacto</Link></p>
       </div>
     </footer>
