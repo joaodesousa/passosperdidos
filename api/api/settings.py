6da616216397682c2c4d855efea61bc0050bb5df
João de Sousa <joao@pedrodesousa.pt>
2025-02-13 03:03:48 +0000

    Removed ALL ORIGINS

--- api/api/settings.py
+++ api/api/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ["4.180.13.143", "localhost", "legis.passosperdidos.pt"]
-#CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://passosperdidos.pt", "https://www.passosperdidos.pt"]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "https://passosperdidos.pt", "https://www.passosperdidos.pt"]
+#CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 
