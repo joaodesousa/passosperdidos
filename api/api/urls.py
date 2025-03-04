816b44889bce1ddb428bd89bdbeaeffaf3835af0
João de Sousa <joao@pedrodesousa.pt>
2025-03-04 18:19:52 +0000

    Added API tokens back.

--- api/api/urls.py
+++ api/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from backend import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 router = DefaultRouter()
 router.register(r'projetoslei', views.ProjetoLeiViewSet)
@@ -17,4 +19,6 @@ urlpatterns = [
     path('dashboard/', views.DashboardStatisticsView.as_view(), name='dashboard-statistics'),
     path('types/', views.TypeListView.as_view(), name='initiative-types'),
     path('phase-names/', views.PhaseListView.as_view(), name='phase-names'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
